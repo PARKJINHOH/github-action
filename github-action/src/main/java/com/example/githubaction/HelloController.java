124c974c8dcbfc6a658950e19a690e3266c4322d
PARK JINHOH <adg0609@easymedia.net>
2023-08-16 09:43:36 +0900

    init

---
+++ github-action/src/main/java/com/example/githubaction/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.githubaction;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping
+    public String Hello(){
+        return "Hello Controller";
+    }
+
+    @GetMapping("/test")
+    public String Test(){
+        return "Hello Controller : Test Method";
+    }
+}
